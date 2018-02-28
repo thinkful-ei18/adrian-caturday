d1cc6623aa8729fc8e4860bee57ae79d5ce7d6bc
Adrian <adriantoddross@gmail.com>
2018-02-28 14:08:11 -0500

    remove redundant role!

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ export default function header(props){
   return (
     <header className="caturday-header" role="banner">
         <h1>It's Caturday!</h1>
-        <p>Make every day special with these GIFs from the <a href="http://thecatapi.com" role="link" >Cat API.</a></p>
+        <p>Make every day special with these GIFs from the <a href="http://thecatapi.com">Cat API.</a></p>
       </header>
   );
 }
