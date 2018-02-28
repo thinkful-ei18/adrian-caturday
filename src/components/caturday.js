138f0319f172516aa08fd4f75830491e6d8f86f8
Adrian <adriantoddross@gmail.com>
2018-02-28 14:30:26 -0500

    add class function for next image: console.log button click

--- src/components/caturday.js
+++ src/components/caturday.js
@@ -5,12 +5,16 @@ import Button from './button';
 
 export default class Caturday extends React.Component {
 
+  nextImage = () => {
+    console.log('`See Another!` was clicked.');
+  }
+
   render () {
     return (
     <main className="caturday" role="main">
       <Header />
       <CaturdayImage tagline="This one's on the house. Check it out!" src="http://thecatapi.com/api/images/get?format=src&type=gif" alt="Random Cat GIF"/>
-      <Button title="See Another" whenClicked={() => {console.log('`See Another!` was clicked.')}}/>
+      <Button title="See Another" whenClicked={this.nextImage}/>
     </main>
     );
 
