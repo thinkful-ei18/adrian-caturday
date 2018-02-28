36f2174ac6e72738ee74038f5f6f46fc57b9a5d0
Adrian <adriantoddross@gmail.com>
2018-02-28 11:48:01 -0500

    test main component render

---
+++ src/components/caturday.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Caturday extends React.Component {
+  render () {
+    return <h1>Hello World!</h1>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import Caturday from './components/caturday';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Caturday />, document.getElementById('root'));
 registerServiceWorker();
