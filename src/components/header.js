bcff86827ef18ea4308487e38959f9869f87a9a8
Adrian <adriantoddross@gmail.com>
2018-02-28 12:31:57 -0500

    add header component; transfer HTML from caturday.js

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function header(props){
+  return (
+    <header>
+        <h1>It's Caturday!</h1>
+        <p>Make every day special with these GIFs from the <a href="http://thecatapi.com">Cat API.</a></p>
+      </header>
+  );
+}
