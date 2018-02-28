793b2460529d5bb8e884845d6e8195318dd9bf4a
Adrian <adriantoddross@gmail.com>
2018-02-28 12:33:41 -0500

    import header.js

--- src/components/caturday.js
+++ src/components/caturday.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import Button from './button';
+import Header from './header';
 import CaturdayImage from './caturday-image';
+import Button from './button';
+
 export default class Caturday extends React.Component {
 
   render () {
     return (
     <main className="caturday" role="main">
-      <header>
-        <h1>It's Caturday!</h1>
-        <p>Make every day special with these GIFs from the <a href="http://thecatapi.com">Cat API.</a></p>
-      </header>
+      <Header />
       <CaturdayImage href="http://thecatapi.com" src="http://thecatapi.com/api/images/get?format=src&type=gif" alt="Random Cat GIF"/>
       <Button title="See Another"/>
     </main>
