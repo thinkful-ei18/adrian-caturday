ea0cbca49583bcbd7f60b6e78f744465d84f1f58
Adrian <adriantoddross@gmail.com>
2018-02-28 12:36:08 -0500

    add className and aria role

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function header(props){
   return (
-    <header>
+    <header className="caturday-header" role="banner">
         <h1>It's Caturday!</h1>
         <p>Make every day special with these GIFs from the <a href="http://thecatapi.com">Cat API.</a></p>
       </header>
