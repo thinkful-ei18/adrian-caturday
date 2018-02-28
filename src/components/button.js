2bf278f322435fe61048231aff742ec3e209bcb8
Adrian <adriantoddross@gmail.com>
2018-02-28 12:41:06 -0500

    add className and aria roles

--- src/components/button.js
+++ src/components/button.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function button(props) {
   return (
-    <button>{props.title}</button>
+    <button input type="button" aria-label={props.title}>{props.title}</button>
   );
 }
