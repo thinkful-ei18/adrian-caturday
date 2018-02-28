b6e9119a022e3a43f86236048f7be4dd692484d8
Adrian <adriantoddross@gmail.com>
2018-02-28 14:04:18 -0500

    remove improper input type

--- src/components/button.js
+++ src/components/button.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function button(props) {
   return (
-    <button input type="button" aria-label={props.title}>{props.title}</button>
+    <section className="caturday-button">
+    <input type="button"/>
+    <button aria-label={props.title}>{props.title}</button>
+    </section>
   );
 }
