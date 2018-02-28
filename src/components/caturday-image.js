d130ec98da803164d767faa669fc62a0c5f04d91
Adrian <adriantoddross@gmail.com>
2018-02-28 15:02:15 -0500

    add default tagline

--- src/components/caturday-image.js
+++ src/components/caturday-image.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
+
 export default function CaturdayImage(props) {
+
+  CaturdayImage.defaultProps = {
+    tagline: "This one's on the house. Check it out!"
+  }
+
   return (
     <section className="caturday-image">
     <img src="http://thecatapi.com/api/images/get?format=src&type=gif" alt="Random Cat GIF"/>
