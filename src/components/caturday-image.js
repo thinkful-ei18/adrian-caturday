8c90f5fed14d543a1b267f8a8b03c879eb010eab
Adrian <adriantoddross@gmail.com>
2018-02-28 12:42:01 -0500

    update aria-label to be the same as alt tag

--- src/components/caturday-image.js
+++ src/components/caturday-image.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function CaturdayImage(props) {
   return (
-    <a href={props.href}><img src={props.src} alt={props.alt} className="caturday-image" role="img" aria-label="Random Cat Image" /></a>
+    <a href={props.href}><img src={props.src} alt={props.alt} className="caturday-image" role="img" aria-label={props.alt} /></a>
   );
 }
