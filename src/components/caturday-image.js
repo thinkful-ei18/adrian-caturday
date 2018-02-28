1365b6fc2936be3fbf1b7872874226d82cb37840
Adrian <adriantoddross@gmail.com>
2018-02-28 12:21:00 -0500

    add props

--- src/components/caturday-image.js
+++ src/components/caturday-image.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function CaturdayImage(props) {
   return (
-    <a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif" alt="Random Cat GIF" /></a>
+    <a href="{props.href}"><img src="{props.src}" alt="{props.alt}" /></a>
   );
 }
