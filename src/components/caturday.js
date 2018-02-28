4e7aaee84f250e8c2b413676058165b930fdb3c9
Adrian <adriantoddross@gmail.com>
2018-02-28 12:02:22 -0500

    test HTML layout

--- src/components/caturday.js
+++ src/components/caturday.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 export default class Caturday extends React.Component {
   render () {
-    return <h1>Hello World!</h1>
+    return (
+    <main className="caturday" role="main">
+      <header>
+        <h1>It's Caturday!</h1>
+        <p>Make every day special with these GIFs.</p>
+      </header>
+      <a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif" alt="Random Cat GIF" /></a>
+      <button>See Another</button>
+    </main>
+    );
+
+
   }
 }
