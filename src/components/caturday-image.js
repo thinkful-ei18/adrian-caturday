174af0b169794aaafec2f6ce195c598fbe5e5294
Adrian <adriantoddross@gmail.com>
2018-02-28 12:19:08 -0500

    copy html from caturday.js

---
+++ src/components/caturday-image.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CaturdayImage(props) {
+  return (
+    <a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif" alt="Random Cat GIF" /></a>
+  );
+}
