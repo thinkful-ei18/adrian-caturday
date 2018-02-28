239b409b2b7ffa3af63cba0777569f6eb6b9ff1f
Adrian <adriantoddross@gmail.com>
2018-02-28 17:08:17 -0500

    add further image randomization

--- src/components/caturday.js
+++ src/components/caturday.js
@@ -8,12 +8,13 @@ export default class Caturday extends React.Component {
     super(props);
     this.state = {
       initialTagline: "This one's on the house. Check it out!",
-      initialImage: "http://thecatapi.com/api/images/get?api_key=Mjc3Mzgz&format=src&type=gif"
+      initialImage: "http://thecatapi.com/api/images/get?&format=src&type=gif"
     }
   }
 
   seeAnother = (props) => {
     // API KEY: Mjc3Mzgz
+    const API_KEY = "Mjc3Mzgz";
 
     const categories = [
       "hats",
@@ -22,18 +23,20 @@ export default class Caturday extends React.Component {
       "funny",
       "boxes",
       "caturday",
-      "kittens",
       "sinks",
       "clothes",
     ]
 
-    let category =categories[Math.floor(Math.random() * 9)]
+    const category = categories[Math.floor(Math.random() * 8)]
 
-    const url = `http://thecatapi.com/api/images/get?api_key=Mjc3Mzgz&format=src&type=gif&category=${category}`
+    const url = `http://thecatapi.com/api/images/get?api_key=${API_KEY}&format=src&type=gif&category=${category}`
+    const url2 = 'http://thecatapi.com/api/images/get?api_key=Mjc3Mzgz&format=src&type=gif'
+    const links = [url, url2]
+    const randomLink = links[Math.floor(Math.random() * 2)];
 
     return (
       this.setState({initialTagline: "Are you not entertained?!"}),
-      this.setState({initialImage: url })
+      this.setState({initialImage: randomLink })
     );
   }
 
